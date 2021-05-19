2ffcff3000a85fc064897eeec6fe05d401a7420a
geiver12 <sosdani2000@hotmail.com>
2021-05-19 00:37:51 -0400

    Updates

--- src/config/apollo.js
+++
@@ -1,9 +0,0 @@
-// config/apollo.js
-
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-export const client = new ApolloClient({
-  uri: 'https://rickandmortyapi.com/graphql', // por defecto la URL seria '/graphql'
-  cache: new InMemoryCache()
-});
-
